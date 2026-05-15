84a820437d01ff623c0714e6ec5a730d5f82ed2a
Urvish Vagdiya <urvish@gmail.com>
2026-05-15 09:35:19 +0530

    complete project

--- backend/controllers/ride.controller.js
+++ backend/controllers/ride.controller.js
@@ -94,9 +94,7 @@ module.exports.startRide = async (req, res) => {
 
     try {
         const ride = await rideService.startRide({ rideId, otp, captain: req.captain });
-
-        console.log(ride);
-
+        
         sendMessageToSocketId(ride.user.socketId, {
             event: 'ride-started',
             data: ride
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -54,7 +54,6 @@ const Home = () => {
     })
 
     socket.on('ride-started', ride => {
-        console.log("ride")
         setWaitingForDriver(false)
         navigate('/riding', { state: { ride } }) // Updated navigate to include ride data
     })
